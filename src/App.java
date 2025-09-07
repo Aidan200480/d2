3fc266dad6df50f4549cdf3887ab38ce468918f3
Aidan McCormac <aidan200480@gmail.com>
2025-09-07 18:05:44 -0400

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in); // This is where the Scanner is defined as sc
+        int num1 = 10; // Interger #1
+        int num2 = 37; // Interger #2
+        System.out.println("How old are you?"); // Asks User For Age
+        String Age = sc.nextLine(); // Age Variable to store users age
+        System.out.println("You are " + (Age) + " years old."); // Prints user age as a message
+        System.out.println("The Sum Of The Two Given Numbers is: " + (num1 + num2)); // Combines the two intergers with a message
+
+
+    }
+}
+// I've learned how to make string and interger variables
+// I've also learned how to import the scanner and define it
+// I've also learned how to do simple addition and subtraction as well as creating sentence using variables
+// Aidan McCormac, 9/7/2025, GitHub Submission
